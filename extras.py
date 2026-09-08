46a439333a37fdb4617bc36794b74fc87a1b25d1
Rafael Hurtarte <25000123@galileo.edu>
2026-09-07 20:59:57 -0600

    Solucion merge conflict

--- extras.py
+++ extras.py
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("HELLO WORD, I AM RAFAEL");
-=======
-        System.out.println("HELLO MUNDO");
->>>>>>> 1faf157c2792e620fed30b2b30bcb02cc87b5c29
     }
 }
